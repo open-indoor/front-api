7b0140b5d8e378effb2aa4ca94737f14bdcb5073
Debian <debian@debian10>
2021-01-11 08:05:26 +0000

    fix caddy setup

--- front-api.sh
+++ front-api.sh
@@ -5,7 +5,9 @@ set -e
 export CADDYPATH=/data/caddy
 export API_SERVER=${API_SERVER:-${API_DOMAIN_NAME}}
 export API_URL=${API_URL:-https://${API_DOMAIN_NAME}}
-export CADDY_TLS=${CADDY_TLS:-'tls contact@openindoor.io { ca {$CERTIFICATE_AUTHORITY} }'}
+export CADDY_TLS=${CADDY_TLS:-'tls contact@openindoor.io {
+    ca {$CERTIFICATE_AUTHORITY}
+}'}
 
 env
 
